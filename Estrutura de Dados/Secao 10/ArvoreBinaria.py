8a152ccd745ca7541e30ab991c8f72081ab9cd10
Lucasrc22 <lucasrios2210@gmail.com>
2025-01-21 09:12:14 -0300

    add insersao de arvore

--- Estrutura de Dados/Secao 10/ArvoreBinaria.py
+++ Estrutura de Dados/Secao 10/ArvoreBinaria.py
@@ -10,3 +10,40 @@ class No:
 class ArvoreBuscaBinaria:
     def __init__(self):
         self.raiz = None
+
+    def inserir(self, valor):
+        novo = No(valor)
+
+        #se a arvore estiver vazia
+
+        if self.raiz == None:
+            self.raiz = novo
+        else:
+            atual = self.raiz
+            while True:
+                pai = atual
+                #esquerdaa
+                if valor < atual.valor:
+                    atual= atual.esquerda
+                    if atual == None:
+                        pai.esquerda = novo
+                        return
+                #direita
+                else:
+                    atual = atual.direita
+                    if atual == None:
+                        pai.direita = novo
+                        return
+
+arvore = ArvoreBuscaBinaria()
+arvore.inserir(53)
+arvore.inserir(54)
+arvore.inserir(23)
+arvore.inserir(87)
+arvore.inserir(98)
+arvore.inserir(22)
+arvore.inserir(12)
+arvore.inserir(11)
+arvore.inserir(10)
+
+print(arvore.raiz.esquerda.valor)
