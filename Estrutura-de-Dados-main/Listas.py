dad84dccca8b8b61d1e1ae40b16eb6b7735185e7
Lucasrc22 <lucasrios2210@gmail.com>
2025-03-17 11:30:35 -0300

    ordenação

--- Estrutura-de-Dados-main/Listas.py
+++ Estrutura-de-Dados-main/Listas.py
@@ -5,10 +5,8 @@ class No:
         self.nome = nome
         self.proximo = None
     
-
     def mostrar_no(self):
-        print(f'Matrícula: {self.matricula}, Nome: {self.nota}, Nota: {self.nome}')
-
+        print(f'Matrícula: {self.matricula}, Nome: {self.nome}, Nota: {self.nota}')
 
 class ListaEncadeada:
     def __init__(self):
@@ -16,23 +14,22 @@ class ListaEncadeada:
         self.ultimo = None
 
     def inserir(self, matricula, nota, nome):
-        novo = No(matricula, nome, nota)
+        novo = No(matricula, nota, nome)
         if self.primeiro is None:
             self.primeiro = novo
             self.ultimo = novo
         else:
-
             atual = self.primeiro
             while atual.proximo is not None:
                 atual = atual.proximo
             atual.proximo = novo
+
     def printAluno(self):
         atual = self.primeiro
-
         while atual is not None:
-            atual.mostrar_no() 
+            atual.mostrar_no()
             atual = atual.proximo
-    
+
     def excluir_inicio(self):
         if self.primeiro is None:
             print("Lista está vazia")
@@ -43,25 +40,24 @@ class ListaEncadeada:
 
     def ordenar_por_nota(self):
         if self.primeiro is None or self.primeiro.proximo is None:
-            return
-
-        trocar = True
-
-        while trocar:
-            trocar = False
+            return  
+        
+        trocado = True
+        while trocado:
+            trocado = False
             atual = self.primeiro
             while atual.proximo is not None:
-                atual.matricula, atual.proximo.matricula = atual.proximo.matricula, atual.matricula
-                atual.nota, atual.proximo.nota = atual.proximo.nota, atual.nota
-                
-                atual.nome, atual.proximo.nome = atual.proximo.nome, atual.nome
-                trocar = True
-            atual = atual.proximo
+                if atual.nota < atual.proximo.nota:
+                    atual.matricula, atual.proximo.matricula = atual.proximo.matricula, atual.matricula
+                    atual.nota, atual.proximo.nota = atual.proximo.nota, atual.nota
+                    atual.nome, atual.proximo.nome = atual.proximo.nome, atual.nome
+                    trocado = True
+                atual = atual.proximo
 
 Aluno = ListaEncadeada()
-Aluno.inserir(1234,10, "fulano")
-Aluno.inserir(5678,8, "ciclano")
-Aluno.inserir(9876,7, "random")
+Aluno.inserir(1234, 10, "Fulano")
+Aluno.inserir(5678, 8, "Ciclano")
+Aluno.inserir(9876, 7, "Random")
 Aluno.inserir(4321, 9, "Beltrano")
 
 print("Lista antes da ordenação:")
@@ -71,9 +67,7 @@ Aluno.ordenar_por_nota()
 
 print("\nLista após ordenação por nota:")
 Aluno.printAluno()
-print("\nExcluindo primeiro aluno")
+
 Aluno.excluir_inicio()
+print("\nLista com o primeiro aluno da nota 10 excluido\n")
 Aluno.printAluno()
-
-
-
